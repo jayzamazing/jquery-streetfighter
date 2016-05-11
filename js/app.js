e57ab1b1c862c7d473d349fcff51268762488fed
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-05-11 00:54:15 -0400

    Added pointsofauthority.mp3, added streetfighter.mpeg, added both to index.html, added content section to index.html. Added introduction, content, streetfighter, fade, name, anim, anim2, and bywhom classes to main.css. Minor positional changes to main.css. Added music to play at page load in app.js.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
 var xDown = false;
 $(document).ready(function() {
-  $('.ryu').mouseenter(function(){
+  playMusic('#points');
+  $('.ryu, .hulk-ryu').mouseenter(function(){
     $('.ryu-hide').hide();
-    $('.ryu-ready').show();
+    $('.ryu-ready, .hulk-ryu-ready').show();
   })
   .mouseleave(function(){
     $('.ryu-hide').hide();
-    $('.ryu-still').show();
+    $('.ryu-still, .hulk-ryu-still').show();
   })
   .mousedown(function(){
     playMusic('#hadouken-sound');
     $('.ryu-hide').hide();
-    $('.ryu-throwing').show();
-    $('.hadouken').finish().show().animate(
+    $('.ryu-throwing, .hulk-ryu-throwing').show();
+    $('.hadouken, .hulk-hadouken').finish().show().animate(
       {'left': '1025px'},
       500,
       function() {
@@ -23,24 +24,26 @@ $(document).ready(function() {
   })
   .mouseup(function(){
     $('.ryu-hide').hide();
-    $('.ryu-ready').show();
+    $('.ryu-ready, .hulk-ryu-ready').show();
   });
 })
 .keydown(function(event) {
     if (xDown)
       return;
     if (event.which == 88){
+      stopMusic('#points');
       xDown = true;
       playMusic('#numb');
       $('.ryu-hide').hide();
-      $('.ryu-cool').show();
+      $('.ryu-cool, .hulk-ryu-cool').show();
   }
 })
 .keyup(function() {
   xDown = false;
   stopMusic('#numb');
+  stopMusic('#points');
   $('.ryu-hide').hide();
-  $('.ryu-still').show();
+  $('.ryu-still, .hulk-ryu-still').show();
 });
 function playMusic (music) {
   $(music)[0].volume = 0.5;
